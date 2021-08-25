f2b6644e20b2f9934c0963b9a07c1b69c61bd08d
Akbar Dhanaliwala <akbar.dhanaliwala@gmail.com>
2021-08-25 12:56:31 -0700

    increase timeout time for tests

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-timeout = time.time() + 15
+timeout = time.time() + 30
 with serial.Serial('/dev/ttyUSB2', 115200, timeout=1) as ser:
     while(((x := ser.readline()) != b'Hello world!\r\n') and (t := (timeout > time.time()))):
         print(x)
